9ac5cadf6e279eced7182e1992744eb43f0ea2fa
martinamancuso <martinamancuso24@gmail.com>
2024-04-08 11:57:40 +0200

    TrisBoardComputer added

--- devplayground/src/App.jsx
+++ devplayground/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, Link } from "react-router-dom";
 import { Tris } from "./tris/Tris";
 import { TrisMenu } from "./tris/TrisMenu/TrisMenu";
 import { TrisBoard } from "./tris/TrisBoard/TrisBoard";
+import { TrisBoardComputer } from "./tris/TrisBoard/TrisBoardComputer";
 
 export function App() {
   return (
@@ -9,6 +10,7 @@ export function App() {
       <Route path="/" element={<Link to="/tris">Tris</Link>} />
       <Route path="tris" element={<Tris />}>
         <Route index element={<TrisMenu />} />
+        <Route path="TrisBoardComputer" element={<TrisBoardComputer />} />
         <Route path="game" element={<TrisBoard />} />
       </Route>
     </Routes>
