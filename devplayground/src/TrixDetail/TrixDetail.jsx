ee7a594437d2f758ac79cde7fde363c34f59e6b0
martinamancuso <martinamancuso24@gmail.com>
2024-04-13 17:06:36 +0200

    trix gif added

--- devplayground/src/TrixDetail/TrixDetail.jsx
+++ devplayground/src/TrixDetail/TrixDetail.jsx
@@ -1,10 +1,13 @@
 import style from "./TrixDetail.module.css"
+import TrixGif from "./trix.gif"
 
 export function TrixDetail() {
   return (
     <div className={style.container}>
-      <div className={style.img}>Image</div>
-      <div className={style.title}>Title</div>
+      <div className={style.img}>
+        <img src={TrixGif} alt="Trix Gif" width="450" height="300" />
+      </div>
+      <div className={style.title}>TRIX</div>
       <div className={style.description}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam consequuntur nesciunt iste impedit, obcaecati iure natus adipisci culpa neque praesentium ea fugit maiores earum, sapiente ullam accusamus vitae aspernatur labore?</div>
     </div>
   );
