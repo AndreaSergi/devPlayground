2328aa1680e2c468b2328a0a91a4d838d9fddbc3
martinamancuso <martinamancuso24@gmail.com>
2024-04-15 17:49:50 +0200

    sizes of GameDetail changed

--- devplayground/src/HOMEPAGE/Homepage.jsx
+++ devplayground/src/HOMEPAGE/Homepage.jsx
@@ -12,20 +12,16 @@ export function Homepage() {
         description={
           <div>
             <p>
-              Experience the thrill of Ping Pong, <br />
-              the virtual game available on PC and mobile.
+              Experience the thrill of Ping Pong, the virtual game available on
+              PC and mobile.
             </p>
-            <br />
             <p>
-              With intuitive controls and realistic gameplay, <br />
-              you can challenge your opponent: <br />
-              move the bar and don't let go the ball.
+              With intuitive controls and realistic gameplay, you can challenge
+              your opponent: move the bar and don't let go the ball.
             </p>
-            <br />
             <p>
-              You will improve your skills until
-              <br />
-              you become a Ping Pong champion.
+              You will improve your skills until you become a Ping Pong
+              champion.
             </p>
           </div>
         }
@@ -36,18 +32,16 @@ export function Homepage() {
         description={
           <div>
             <p>
-              Be the first to get three symbols in a row, <br />
-              either horizontally, vertically, or diagonally.
+              Be the first to get three symbols in a row, either horizontally,
+              vertically, or diagonally.
             </p>
-            <br />
             <p>
-              Trix is a classic and fun game played on a 3x3 grid, <br />
-              where players take turns placing 'X' and 'O'.
+              Trix is a classic and fun game played on a 3x3 grid, where players
+              take turns placing 'X' and 'O'.
             </p>
-            <br />
             <p>
-              The game ends when all squares are filled, <br />
-              either with a winner or a draw.
+              The game ends when all squares are filled, either with a winner or
+              a draw.
             </p>
           </div>
         }
@@ -59,15 +53,12 @@ export function Homepage() {
         description={
           <div>
             <p>
-              Find the right word with Wordify, <br />
-              the captivating word puzzle game you can play <br />
-              on your PC or mobile device.
+              Find the right word with Wordify, the captivating word puzzle game
+              you can play on your PC or mobile device.
             </p>
-            <br />
             <p>
-              Five-letter word within six tries: <br />
-              with its minimalist design and brain-teasing puzzles, <br />
-              Wordle is perfect for your daily fun.
+              Five-letter word within six tries: with its minimalist design and
+              brain-teasing puzzles, Wordle is perfect for your daily fun.
             </p>
           </div>
         }
--- devplayground/src/HOMEPAGE/SECTIONS/GameDetail/GameDetail.jsx
+++ devplayground/src/HOMEPAGE/SECTIONS/GameDetail/GameDetail.jsx
@@ -2,12 +2,14 @@ import style from "./GameDetail.module.css";
 
 export function GameDetail({ img, title, description }) {
   return (
-      <div className={style.container}>
-        <div className={style.img}>
-          <img src={img} alt="Game Detail Gif" width="450" height="300" />
-        </div>
-        <div className={`${style.title} ${style.modakRegular}`}>{title}</div>
+    <div className={style.container}>
+      <div className={style.img}>
+        <img src={img} alt="Game Detail Gif" />
+      </div>
+      <div className={style.info}>
+        <div className={`${style.title} modak-regular`}>{title}</div>
         <div className={style.description}>{description}</div>
       </div>
+    </div>
   );
 }
