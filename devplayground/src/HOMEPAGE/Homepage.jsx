25e805d2db04d5ed4a9ec7efcba4ba17b159011e
martinamancuso <martinamancuso24@gmail.com>
2024-04-15 15:15:23 +0200

    games description updated

--- devplayground/src/HOMEPAGE/Homepage.jsx
+++ devplayground/src/HOMEPAGE/Homepage.jsx
@@ -10,22 +10,22 @@ export function Homepage() {
         img={TrixGif}
         title={"TRIX"}
         description={
-          <pre>
+          <div>
             <p>
-            TRIX is a classic and fun game played on a 3x3 grid, <br />
-            where players take turns placing 'X' and 'O'.
+              Be the first to get three symbols in a row, <br />
+              either horizontally, vertically, or diagonally.
             </p>
             <br />
             <p>
-              The aim is to be the first to get three symbols in a row, <br />
-              either horizontally, vertically, or diagonally.
+              TRIX is a classic and fun game played on a 3x3 grid, <br />
+              where players take turns placing 'X' and 'O'.
             </p>
             <br />
             <p>
               The game ends when all squares are filled, <br />
               either with a winner or a draw.
             </p>
-          </pre>
+          </div>
         }
       />
 
@@ -33,22 +33,23 @@ export function Homepage() {
         img={TrixGif}
         title={"TRIX"}
         description={
-          <pre>
+          <div>
             <p>
-            TRIX is a classic and fun game played on a 3x3 grid, <br />
-            where players take turns placing 'X' and 'O'.
+              Experience the thrill of Ping Pong, <br />
+              the virtual game available on PC and mobile.
             </p>
             <br />
             <p>
-              The aim is to be the first to get three symbols in a row, <br />
-              either horizontally, vertically, or diagonally.
+              With intuitive controls and realistic gameplay, <br />
+              you can challenge your opponent: <br />
+              move the bar and don't let go the ball.
             </p>
             <br />
             <p>
-              The game ends when all squares are filled, <br />
-              either with a winner or a draw.
+              You will improve your skills until<br />
+              you become a Ping Pong champion.
             </p>
-          </pre>
+          </div>
         }
       />
 
@@ -56,22 +57,19 @@ export function Homepage() {
         img={TrixGif}
         title={"TRIX"}
         description={
-          <pre>
+          <div>
             <p>
-            TRIX is a classic and fun game played on a 3x3 grid, <br />
-            where players take turns placing 'X' and 'O'.
+              Find the right word with WORDIFY, <br />
+              the captivating word puzzle game you can play <br />
+              on your PC or mobile device.
             </p>
             <br />
             <p>
-              The aim is to be the first to get three symbols in a row, <br />
-              either horizontally, vertically, or diagonally.
+              Five-letter word within six tries: <br />
+              with its minimalist design and brain-teasing puzzles, <br />
+              Wordle is perfect for your daily fun.
             </p>
-            <br />
-            <p>
-              The game ends when all squares are filled, <br />
-              either with a winner or a draw.
-            </p>
-          </pre>
+          </div>
         }
       />
     </>
