b78ba943ce9482f561303b5331468b516f145055
martinamancuso <martinamancuso24@gmail.com>
2024-04-15 14:08:36 +0200

    added properties to TrixDetail

--- devplayground/src/HOMEPAGE/Homepage.jsx
+++ devplayground/src/HOMEPAGE/Homepage.jsx
@@ -1,16 +1,67 @@
 import { MainSection } from "./SECTIONS/MAIN_SECTION/MainSection";
 import { TrixDetail } from "./SECTIONS/TrixDetail/TrixDetail";
+import TrixGif from "./SECTIONS/TrixDetail/trix.gif";
 
-export function Homepage(){
-
-
-
-    return(
+export function Homepage() {
+  return (
     <>
-        <MainSection/>
-        <TrixDetail/>
-        <TrixDetail/>
-        <TrixDetail/>
+      <MainSection />
+      <TrixDetail
+        img={TrixGif}
+        title={"TRIX"}
+        description={
+          <pre>
+            TRIX is a classic and fun game played on a 3x3 grid, <br />
+            where players take turns placing 'X' and 'O'.
+            <p>
+              The aim is to be the first to get three symbols in a row, <br />
+              either horizontally, vertically, or diagonally.
+            </p>
+            <p>
+              The game ends when all squares are filled, <br />
+              either with a winner or a draw.
+            </p>
+          </pre>
+        }
+      />
+
+      <TrixDetail
+        img={TrixGif}
+        title={"TRIX"}
+        description={
+          <pre>
+            TRIX is a classic and fun game played on a 3x3 grid, <br />
+            where players take turns placing 'X' and 'O'.
+            <p>
+              The aim is to be the first to get three symbols in a row, <br />
+              either horizontally, vertically, or diagonally.
+            </p>
+            <p>
+              The game ends when all squares are filled, <br />
+              either with a winner or a draw.
+            </p>
+          </pre>
+        }
+      />
+
+      <TrixDetail
+        img={TrixGif}
+        title={"TRIX"}
+        description={
+          <pre>
+            TRIX is a classic and fun game played on a 3x3 grid, <br />
+            where players take turns placing 'X' and 'O'.
+            <p>
+              The aim is to be the first to get three symbols in a row, <br />
+              either horizontally, vertically, or diagonally.
+            </p>
+            <p>
+              The game ends when all squares are filled, <br />
+              either with a winner or a draw.
+            </p>
+          </pre>
+        }
+      />
     </>
-)
+  );
 }
