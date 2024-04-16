a86aaa9d569972d41a90e2f02046abbc4eeb1814
Giampiero <giampierof2011@hotmail.it>
2024-04-16 14:50:11 +0200

    Revert "fine"
    This reverts commit 4da7d2864a8c135338adb3754cf72566569e088f.

--- devplayground/src/Login/Login.jsx
+++ devplayground/src/Login/Login.jsx
@@ -3,23 +3,11 @@ import Form from 'react-bootstrap/Form';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import GetRegister from "../Register/register";
-import { useRef, useState } from 'react';
-
-import { useNavigate } from "react-router-dom";
-
-
+/*
+<div class="p-3 mb-2 bg-dark bg-gradient text-white">.bg-dark.bg-gradient</div>
+*/
 
 export function Login() {
-
-    const email = useRef(null);
-    const pass = useRef(null);
-    const [metodi, setMetodi] = useState(new GetRegister());
-
-    let isLogged=false;
-
-    const nav = useNavigate();
-
     return (
         <div className='p-3 mb-2 bg-black text-white' style={
             {
@@ -34,8 +22,8 @@ export function Login() {
         }>
             <div className='p-3 mb-2 bg-dark bg-gradient text-white' style={
                 {
-                    minWidth: "40vw",
-                    minHeight: "70vh",
+                    width: "40vw",
+                    height: "70vh",
                     borderRadius: "16px",
                     display: "flex",
                     flexDirection: "column",
@@ -47,48 +35,29 @@ export function Login() {
                 </div>
                 <Form style={
                     {
-                        padding: "16px",
+                        padding:"16px",
                         display: "flex",
                         flexDirection: "column",
                         justifyContent: "center",
                     }
                 }>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label> <h4>Indirizzo e-mail</h4> </Form.Label>
-                        <Form.Control type="email" placeholder="prova.example@devplayground.com" ref={email} />
+                        <Form.Label> <h4>Email address</h4> </Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
                         <Form.Text className="text-muted">
                         </Form.Text>
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label> <h4>Password</h4> </Form.Label>
-                        <Form.Control type="password" placeholder="Password" ref={pass} />
+                        <Form.Control type="password" placeholder="Password" />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     </Form.Group>
-                    <Button variant="primary" type="submit" onClick={
-                        (event) => {
-                            event.preventDefault();
-                            console.log("mail: " + email.current.value);
-                            console.log("pass: " + pass.current.value);
-                            isLogged = metodi.getData(metodi.checkData(email.current.value), email.current.value, pass.current.value);
-                            if (isLogged) {
-                                sessionStorage.setItem("isLogged","true");
-                                nav("/");
-                            } else {
-                                sessionStorage.setItem("isLogged","false");
-                            }
-                            
-                        }
-                    }>
+                    <Button variant="primary" type="submit">
                         Accedi
                     </Button>
                 </Form>
-
-                <div style={{ textAlign: "center" }}>
-                    <h6>Non sei registrato?</h6>
-                    <h8 className={"text-primary"} onClick={() => { nav("/register") }} style={{ cursor: 'pointer' }}>Clicca qui per farlo!</h8>
-                </div>
 
             </div>
 
--- devplayground/src/Register/Register.jsx
+++ devplayground/src/Register/Register.jsx
@@ -2,20 +2,12 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useEffect, useRef, useState } from 'react';
-import { useNavigate } from "react-router-dom"
 
-import GetRegister from "./register";
-
+/*
+<div class="p-3 mb-2 bg-dark bg-gradient text-white">.bg-dark.bg-gradient</div>
+*/
 
 export function Register() {
-
-    const email = useRef(null);
-    const pass = useRef(null);
-    const [metodi, setMetodi] = useState(new GetRegister());
-
-    const nav=useNavigate();
-
     return (
         <div className='p-3 mb-2 bg-black text-white' style={
             {
@@ -30,8 +22,8 @@ export function Register() {
         }>
             <div className='p-3 mb-2 bg-dark bg-gradient text-white' style={
                 {
-                    minWidth: "40vw",
-                    minHeight: "70vh",
+                    width: "40vw",
+                    height: "70vh",
                     borderRadius: "16px",
                     display: "flex",
                     flexDirection: "column",
@@ -43,38 +35,26 @@ export function Register() {
                 </div>
                 <Form style={
                     {
-                        padding: "16px",
+                        padding:"16px",
                         display: "flex",
                         flexDirection: "column",
                         justifyContent: "center",
                     }
                 }>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label> <h4>Indirizzo e-mail</h4> </Form.Label>
-                        <Form.Control type="email" placeholder="prova.example@devplayground.com" ref={email} />
+                        <Form.Label> <h4>Email address</h4> </Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
                         <Form.Text className="text-muted">
                         </Form.Text>
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label> <h4>Password</h4> </Form.Label>
-                        <Form.Control type="password" placeholder="Password" ref={pass} />
+                        <Form.Control type="password" placeholder="Password" />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     </Form.Group>
-                    <Button variant="primary" type="submit" onClick={
-                        (event) => {
-                            event.preventDefault();
-                            //console.log("mail: " + email.current.value);
-                            //console.log("pass: " + pass.current.value);
-                            if (metodi.setData(metodi.checkData(email.current.value), email.current.value, pass.current.value)) {
-                                nav("/");   // il metodo setData ritorna un booleano : se ritorna true vuol dire che ha regitrato il nuovo
-                                            // utente, quindi viene reindirizzato alla home;
-                            }else{
-                                nav("/login"); //se ritorna false vuol dire che l'utente è già registrato e quindi viene reindirizzato al login.
-                            }
-                        }
-                    }>
+                    <Button variant="primary" type="submit">
                         Registrati
                     </Button>
                 </Form>
