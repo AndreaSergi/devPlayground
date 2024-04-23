d7feceddaa856d0f01cc780914c5ac529e644caf
Davide Lodde <johndoe@example.com>
2024-04-23 09:45:31 +0200

    fix buttons ping pong

--- devplayground/src/pingPong/Button.jsx
+++ devplayground/src/pingPong/Button.jsx
@@ -1,5 +1,18 @@
 /* eslint-disable react/prop-types */
-export function Button({text, className, style}) {
+export function Button({text, className}) {
+
+    const style = {
+        borderRadius: "8px",
+        border: "1px solid trasparent",
+        padding: "0.6em 1.2em",
+        fontSize: "1em",
+        fontWeight: "500",
+        fontFamily: "inherit",
+        cursor: "pointer",
+        transition: "border-color 0.25s",
+        width: "15.9vw"
+    } 
+
     return(
         <button style={style} className={className}>{text}</button>
     )
