c0b6203368a8c014d1bbae7d9de2c24fa07a35f3
Giampiero <giampierof2011@hotmail.it>
2024-04-23 12:04:30 +0200

    fix ping pong

--- devplayground/src/pingPong/Home.jsx
+++ devplayground/src/pingPong/Home.jsx
@@ -1,20 +1,27 @@
 import { Link } from "react-router-dom";
 import { Button } from "./Button";
-import "./Home.css";
+import "./PingPong/Home.css";
 
 
 export function Home() {
 
   const styleLogo = {
-    height: "150px",
-    marginBottom: "80px"
+    marginTop: "10px",
+    marginBottom: "10px",
+    width: "12vw"
+  }
+
+  const styleButton = {
+    width: "200px",
+    height: "50px",
+    marginBottom: "10px"
   }
 
   return (
     <div className="container">
       <img style={styleLogo} src="/pingpong/img/ic.svg"/>
       <Link to="/pvcpu">
-        <Button className="pvcom" text="Player VS CPU" />
+        <Button style={styleButton} className="pvcom" text="Player VS CPU" />
       </Link>
       <Link to="/pvp">
         <Button className="pvp" text="Player VS Player" />
