dbce43370edf7943bbc18cc6c52b6352c9c2f648
martinamancuso <martinamancuso24@gmail.com>
2024-04-27 20:00:17 +0200

    3 options about homepage font added

--- devplayground/src/HOMEPAGE/SECTIONS/MAIN_SECTION/MainSection.jsx
+++ devplayground/src/HOMEPAGE/SECTIONS/MAIN_SECTION/MainSection.jsx
@@ -1,46 +1,41 @@
+export function MainSection() {
+  return (
+    <section className="appContainer">
+      <header className="site-header">
+        <div className="site-header-mask">
+          {/*   <h1 id="animated" className="title modak-regular"> <span className="segno">&lt;</span><span className="dev">dev</span><span className="playground">Playground</span><span className="segno">/&gt;</span></h1>  */}
+          {/* <h1 className='title modak-regular waveTextAnimated'> */}
 
-export function MainSection(){
+          {/* Opzione 1 */}
+          {/* <h1 className="title jersey-15-regular waveTextAnimated"> */}
 
+          {/* Opzione 2 */}
+          {/* <h1 className="title gochi-hand-regular waveTextAnimated"> */}
 
-
-    return (
-        <section className='appContainer'>
-
-            <header className="site-header">
-
-                <div className="site-header-mask">
-
-                   {/*   <h1 id="animated" className="title modak-regular"> <span className="segno">&lt;</span><span className="dev">dev</span><span className="playground">Playground</span><span className="segno">/&gt;</span></h1>  */}
-                   <h1 className='title modak-regular waveTextAnimated'>
-                        <span className="segno">&lt;</span>
-                        <span className="dev">d</span>
-                        <span className="dev">e</span>
-                        <span className="dev">v</span>
-                        <span>P</span>    
-                        <span>l</span>
-                        <span>a</span>
-                        <span>y</span>
-                        <span>g</span>
-                        <span>r</span>    
-                        <span>o</span>    
-                        <span>u</span>
-                        <span>n</span>
-                        <span>d</span>
-                        <span className="segno">/</span>
-                        <span className="segno">&gt;</span>    
-                    </h1>
-{/* 
+          {/* Opzione 3 */}
+          <h1 className="title luckiest-guy-regular waveTextAnimated">
+          
+            <span className="segno">&lt;</span>
+            <span className="dev">d</span>
+            <span className="dev">e</span>
+            <span className="dev">v</span>
+            <span>P</span>
+            <span>l</span>
+            <span>a</span>
+            <span>y</span>
+            <span>g</span>
+            <span>r</span>
+            <span>o</span>
+            <span>u</span>
+            <span>n</span>
+            <span>d</span>
+            <span className="segno">/</span>
+            <span className="segno">&gt;</span>
+          </h1>
+          {/* 
                     <h1 data-text="<devPlayground/>" className="total-title"> <span className="segno">&lt;</span><span className="dev">dev</span><span className="playground">Playground</span><span className="segno">/&gt;</span></h1> */}
-
-                    
-                
-            
-                </div>
-                
-
-            </header>
-
-        
-        </section>
-    )
+        </div>
+      </header>
+    </section>
+  );
 }
